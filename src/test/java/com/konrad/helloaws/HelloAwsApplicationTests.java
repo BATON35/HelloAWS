8885343e3d5dcad704790d1fd4eedb21b43fa55a
Konrad <https://github.com/BATON35>
2026-02-19 00:32:44 +0100

    initial commit

---
+++ src/test/java/com/konrad/helloaws/HelloAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.helloaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
